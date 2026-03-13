f2b381ec1c6c642f14ba5a97e8a812415fd68a51
Oscar Lillo <osergod999@gmail.com>
2026-03-13 20:51:14 +0100

    Termino la actividad 12

--- app/src/main/java/com/example/act3/TableActivity.kt
+++ app/src/main/java/com/example/act3/TableActivity.kt
@@ -2,10 +2,14 @@ package com.example.act3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table)
+
+        val btnReturn = findViewById<Button>(R.id.btnReturnTable)
+        btnReturn.setOnClickListener { finish() } // vuelve a Activity_12
     }
 }
