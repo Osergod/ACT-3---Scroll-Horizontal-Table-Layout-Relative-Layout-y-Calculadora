7b9110232b9cf3d3d49cb94c83fdec5d46cab055
Oscar Lillo <osergod999@gmail.com>
2026-03-13 18:49:59 +0100

    Hago la actividad_11

--- app/src/main/java/com/example/act3/Activity_11.kt
+++ app/src/main/java/com/example/act3/Activity_11.kt
@@ -2,10 +2,29 @@ package com.example.act3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.HorizontalScrollView
+import android.widget.ToggleButton
+import android.widget.Button
+import android.content.Intent
 
 class Activity_11 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_11)
+
+        val toggle = findViewById<ToggleButton>(R.id.toggleScroll)
+        val scroll = findViewById<HorizontalScrollView>(R.id.hScroll)
+
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            scroll.isHorizontalScrollBarEnabled = isChecked
+        }
+
+        val botonVolver = findViewById<Button>(R.id.ReturnButton11)
+
+        botonVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
