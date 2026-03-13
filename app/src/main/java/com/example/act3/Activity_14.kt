cb942c7bcb9fa7cbce455e6576cd469aaffbc342
Oscar Lillo <osergod999@gmail.com>
2026-03-13 18:24:43 +0100

    Creo la MainActivity y las otras 4 pantallas. En la main activity he puesto 4 botones para acceder a las diferentes pantallas

---
+++ app/src/main/java/com/example/act3/Activity_14.kt
@@ -0,0 +1,11 @@
+package com.example.act3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_14 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_14)
+    }
+}
--- app/src/main/java/com/example/act3/MainActivity.kt
+++ app/src/main/java/com/example/act3/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.act3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val act11 = findViewById<Button>(R.id.Act11)
+        val act12 = findViewById<Button>(R.id.Act12)
+        val act13 = findViewById<Button>(R.id.Act13)
+        val act14 = findViewById<Button>(R.id.Act14)
+
+        act11.setOnClickListener {
+            val intent = Intent(this, Activity_11::class.java)
+            startActivity(intent)
+        }
+
+        act12.setOnClickListener {
+            val intent = Intent(this, Activity_12::class.java)
+            startActivity(intent)
+        }
+
+        act13.setOnClickListener {
+            val intent = Intent(this, Activity_13::class.java)
+            startActivity(intent)
+        }
+
+        act14.setOnClickListener {
+            val intent = Intent(this, Activity_14::class.java)
+            startActivity(intent)
+        }
     }
 }
